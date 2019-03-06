3e484fa3a283ac9ce66f26bc45c623c83d52ea6b
j4sysiak <j4sysiak@gmail.com>
2019-03-06 07:05:34 +0100

    11

---
+++ spring-tutorial-PersonFactory/src/main/java/pl/jaceksysiak/spring/test/App.java
@@ -0,0 +1,24 @@
+package pl.jaceksysiak.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	
+	public static void main(String[] args){
+		 
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/jaceksysiak/spring/test/beans/beans.xml");
+		
+		Person person = (Person) context.getBean("person");
+		
+		person.setTaxId(666);
+		
+		System.out.println(person);
+		
+		 
+	 
+		
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
